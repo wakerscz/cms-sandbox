8ba4142b2339e5d1a1616b2956d9871963f4c746
Jiří Zapletal <zapletal@wakers.cz>
2019-06-19 13:56:02 +0200

    Added compact DB dump

--- sc-dump.sh
+++ sc-dump.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose exec mariadb sh -c 'exec mysqldump wakers_cms_db -uroot -p"$MYSQL_ROOT_PASSWORD" > /dumps/"$(date +%Y%m%d-%H%M%S)".sql'
+docker-compose exec mariadb sh -c 'exec mysqldump wakers_cms_db --no-create-info --compact --all-tablespaces --extended-insert=FALSE  -uroot -p"$MYSQL_ROOT_PASSWORD" > /dumps/"$(date +%Y%m%d-%H%M%S)".sql'
